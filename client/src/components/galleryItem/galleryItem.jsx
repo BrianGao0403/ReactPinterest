4995f2a8cc07884ded8f89f2644e1b8225d30050
IntoYou <1105256187@qq.com>
2025-06-03 16:31:54 +1000

    use imagekit

--- client/src/components/galleryItem/galleryItem.jsx
+++ client/src/components/galleryItem/galleryItem.jsx
@@ -1,10 +1,15 @@
 import './galleryItem.css'
 import { Link } from 'react-router';
+import Image from '../image/image';
 
 const GalleryItem = ({item}) => {
+
+    const optimizeHeight = (370 * item.height) / item.width
+
     return (
         <div className='galleryItem'  style={{ gridRowEnd: `span ${Math.ceil(item.height / 100)}` }}>
-            <img src={item.media} alt="" />
+            <Image path={item.media} alt="" w={370} h={optimizeHeight}/>
+           
             <Link to={'/pin/${item.id}'} className='overlay'/>
             <button className='saveButton'>Save</button>
             <div className='overlayIcons'>
